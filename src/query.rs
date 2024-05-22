e0d49927d5aa2056ce0f0ab3bb4ff75066a516ee
Matt Klein <mklein@bitdrift.io>
2024-05-22 12:00:54 -0600

    add debug log for query
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- src/query.rs
+++ src/query.rs
@@ -155,6 +155,7 @@ impl Query {
         default_format: Option<&str>,
     ) -> Result<Response> {
         let query = self.sql.finish()?;
+        log::debug!("executing query: {}", query);
 
         let mut url =
             Url::parse(&self.client.url).map_err(|err| Error::InvalidParams(Box::new(err)))?;
